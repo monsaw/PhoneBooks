d3b1ec4407e83dfefbd1063ccbfd1c2cc51e34b1
monsaw <lawalmonsaw@gmail.com>
2022-05-12 20:42:29 +0100

    "Upadate"

--- src/ByName.java
+++ src/ByName.java
@@ -2,41 +2,26 @@ import java.util.Map;
 
 public class ByName {
     public static void byName(){
-        System.out.print("Enter the name : ");
-        String name = Main.input.next().toLowerCase();
 
-//        Line to check if the name enter is a sunpart of the expected name.
-//        for (Map.Entry names:
-//                Main.details.entrySet())
-//            if ( !names.getKey().toString().equals(name)) {
-//                System.out.println("List of contacts that start with " + name);
-//                break;
-//
-//            }
-        System.out.println("Getting a name means you searched for the exact name , \n" +
-                "Getting multiple names means your search word prefix some of the name in the storage");
+        System.out.print("Enter the name : ");
+
+        String name = Main.input.next().toLowerCase();
+        if (Main.details.containsKey(name)){
+
             for (Map.Entry names:
                     Main.details.entrySet()) {
                 if (names.getKey().equals(name)){
-
                     System.out.println();
-
                     System.out.println("Name : " + names.getKey());
                     System.out.println("Number : " + names.getValue());
-                    break;
-                }
-                else if (names.getKey().toString().startsWith(name)){
-                    System.out.println();
-
-                    System.out.println("Name : " + names.getKey());
-                    System.out.println("Number : " + names.getValue());
-                }
-                else {
-                    System.out.println("Record not found!");
                 }
 
             }
 
-//        }
+        } else {
+            System.out.println("Record not found!");
+
+        }
     }
-}
+    }
+
